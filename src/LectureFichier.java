9a823d5602905622dfb6269e1459496ddfa4e3c7
Alexis Archambault <alexis2731link@gmail.com>
2020-02-18 11:38:40 -0500

    Formatage

--- src/LectureFichier.java
+++ src/LectureFichier.java
@@ -6,47 +6,53 @@ public class LectureFichier {
 	public static void main(String[] args) throws IOException {
 		boolean peutContinuer = true;
 
-		String nomFicTest;
 		String texteFic = "";
 		String line;
-		
-		BufferedReader fic = new BufferedReader( new InputStreamReader( System.in ) );
-		
-		nomFicTest = OutilsFichier.lireNomFichier( "\nEntrez le nom du fichier qui contient les commandes : " );
 
-		fic = OutilsFichier.ouvrirFicTexteLecture( nomFicTest );
-		
-		if ( fic == null ) {
+		BufferedReader fic = lireNomFichier();
+
+		if (fic == null) {
 			peutContinuer = false;
-			
+
 		} else {
 			while ((line = fic.readLine()) != null) {
 				texteFic += line;
+
 			}
-			
+
 			if (!bonFormat(texteFic)) {
 				peutContinuer = false;
+
 				System.out.println("\nLe fichier n'est pas de bon format.");
+
 			}
 		}
-		
-		if ( peutContinuer ) {
 
+		if (peutContinuer) {
 			OutilsLecture.fic = fic;
-			
-			System.out.println( "\nyep" );
 
-			//TODO ajouter la suite
+			// TODO ajouter la suite
 
 		} else {
-			System.out.println( "\nImpossible de tester le programme." );
+			System.out.println("\nImpossible de tester le programme.");
+
 		}
 
 	}
-	
-	public static boolean bonFormat( String texteFic ) {
-		
-		return texteFic.matches("Clients\\ ?:\\ ?[a-zA-ZÀ-ÿ\\ ]*\\ ?Plats\\ ?:\\ ?([a-zA-ZÀ-ÿ]*[0-9\\.\\ \\_]*)*Commandes\\ ? :\\ ?([a-zA-ZÀ-ÿ]*[0-9\\.\\ \\_]*)*Fin");
+
+	public static BufferedReader lireNomFichier() {
+		String nomFicTest;
+
+		nomFicTest = OutilsFichier.lireNomFichier("\nEntrez le nom du fichier qui contient les commandes : ");
+
+		return OutilsFichier.ouvrirFicTexteLecture(nomFicTest);
+
 	}
-	
+
+	public static boolean bonFormat(String texteFic) {
+
+		return texteFic.matches(
+				"Clients\\ ?:\\ ?[a-zA-ZÀ-ÿ\\ ]*\\ ?Plats\\ ?:\\ ?([a-zA-ZÀ-ÿ]*[0-9\\.\\ \\_]*)*Commandes\\ ? :\\ ?([a-zA-ZÀ-ÿ]*[0-9\\.\\ \\_]*)*Fin");
+	}
+
 }
